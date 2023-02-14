f5aa306b553f8cbaabebbc5853641b89c8b2153f
Wessel T <wessel@go2it.eu>
2023-02-14 18:18:12 +0100

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,15 @@
+/* main.c */
+/*
+  Project description
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "../include/utils.h"
+/* Libtap for tests */
+#include "../include/libtap.h"
+
+int main(int argc, char *argv[], char *env[]) {
+  return 0;
+}
